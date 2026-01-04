61ff682679575606c417ff679455ea141907d893
Aryan Koshti <aryankosti120@gmail.com>
2026-01-04 16:21:24 +0530

    Add landing page route components

---
+++ Frontend/src/LandingPage/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <section className="w-full py-24">
+            <div className="max-w-4xl mx-auto text-center px-6">
+                <h1>404 Not Found!!</h1>
+                <p className="text-gray-600 mb-8">
+                    Sorry the page you are looking for doesn't exist.        </p>
+
+            </div>
+        </section>
+    );
+}
+
+export default NotFound
---
+++ Frontend/src/LandingPage/Pricing/PricingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Brokerage from './Brokerage'
+import Hero from './Hero'
+import OpenAccounnt from '../../components/OpenAccount'
+
+function PricingPage() {
+  return (
+    <>
+    <Hero/>
+    <OpenAccounnt />
+    <Brokerage/>
+
+    
+    </>
+  )
+}
+
+export default PricingPage
